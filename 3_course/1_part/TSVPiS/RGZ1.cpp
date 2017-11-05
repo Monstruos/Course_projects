b9e6ba340d17f2d9202bfa2de5e58542771297c6
Monstruos <mongstruos@mail.ru>
2017-11-05 20:00:49 +0700

    Added files of RGZ (TSVPiS)

---
+++ 3_course/1_part/TSVPiS/RGZ1.cpp
@@ -0,0 +1,6 @@
+/*This program is multiplying 2 matrixes by algorithm of fast multiplying:
+A[i, j] = (-1) * (i + j);
+B[i, j] = i - j;
+i, j = 1..100;
+Also, this program is calculating labor of alforithm
+*/
