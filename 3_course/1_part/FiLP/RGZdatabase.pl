382100645f26560c80cc147ca28015425075acc1
Monstruos <mongstruos@mail.ru>
2017-11-21 22:26:51 +0700

    RGZ(FiLP)

--- 3_course/1_part/FiLP/RGZdatabase.pl
+++ 3_course/1_part/FiLP/RGZdatabase.pl
@@ -1,5 +1,12 @@
-plane(3626, moscow, time(12, 30), date(20, 11), 5000).
-plane(2456, las-vegas, time(03, 50), date(21, 11), 10000).
+:- dynamic plane/5.
+
+plane(3625, moscow, time(12, 30), date(20, 11), 5000).
+plane(3626, moscow, time(15, 30), date(20, 11), 7000).
+plane(3627, moscow, time(15, 45), date(20, 11), 5000).
+plane(3628, moscow, time(20, 30), date(20, 11), 8000).
+plane(2456, las-vegas, time(3, 50), date(21, 11), 10000).
 plane(6112, paris, time(14, 11), date(20, 11), 7600).
-plane(9567, novosibirsk, time(06, 00), date(20, 11), 4500).
+plane(9567, novosibirsk, time(6, 0), date(20, 11), 4500).
 plane(1435, berlin, time(15, 20), date(21, 11), 6500).
+plane(7256, leningrad, time(14, 23), date(23, 11), 8650).
+
--- 3_course/1_part/FiLP/RGZdbeditor.pl
+++ 3_course/1_part/FiLP/RGZdbeditor.pl
@@ -15,19 +15,18 @@ menu():-
       write('4-Поиск всех рейсов до города, вылетающих в ближайшие 6 часов с минимальной стоимостью билета'),nl,
       write('5–Выход (и сохранение)'),nl,
       write('--------------------------------'),nl,
-      write('Выберите пункт меню: (1-6) '),
+      write('Выберите пункт меню: (1-5) '),
       read(X),
       X>0,
       X<6,
-      %process(X),
+      process(X),
       X=:=5,!.
 
 process(1):-dbview().
-%process(2):-add_iron,!.
-%process(3):-remove_iron,!.
-%process(4):-db_save_iron,!.
-%process(5):-find_iron,!.
-%process(6):-retractall(iron/3),!.
+process(2):-add(),!.
+process(3):-delete_plane(),!.
+process(4):-find(),!.
+process(5):-tell('RGZdatabase.pl'), listing(plane), told,!.
 
 dbview():-
     write('Просмотр:'), nl,
@@ -36,7 +35,8 @@ dbview():-
     write('Город:'), write(City), nl,
     write('Время:'), write(Hour), write(':'), write(Min), nl,
     write('Дата:'), write(Day), write('-'), write(Month), nl,
-    write('Стоимость:'), write(Cost), nl.
+    write('Стоимость:'), write(Cost), nl,
+    write('========================'), nl.
 
 add():-
     write('Добавить рейс:'), nl,
@@ -58,13 +58,6 @@ add():-
     assertz(plane(Flight, City, time(Hour, Min), date(Day, Month), Cost)),
     next(),!.
 
-next():-
-    write('Выход? (q для выхода)'),
-    read(A),
-    exit(A).
-
-exit(_):-fail.
-exit(q).
 
 delete_plane():-
     write('Удаление рейса(по номеру):'), nl,
@@ -74,59 +67,96 @@ delete_plane():-
     retract(plane(F, _, _, _, _)),
     next(),!.
 
+next():-
+    write('Выход? (q для выхода)'),
+    read(A),
+    exit(A).
+
+exit(_):-fail.
+exit(q).
+
 
 find():-
-    write('Введите время (час)'),
+    write('Введите город'),
+    read(City),
+    write('Введите текущее время (час)'),
     read(T1),
-    write('Введите время (минута)'),
+    write('Введите текущее время (минута)'),
     read(T2),
-    write('Введите дату (день)'),
+    write('Введите текущую дату (день)'),
     read(D1),
-    write('Введите дату (месяц)'),
+    write('Введите текущую дату (месяц)'),
     read(D2),
-    list(time(T1, T2), date(D1, D2), L),
-    L = [0].
-
-list(time(H, M), date(D, Mon), L):-
-    plane(F, City, time(H1, M1), date(D1, Mon1), Cost),
-    comp(time(H, M), date(D, Mon), time(H1, M1), date(D1, Mon1), Res),
-    Res >= 0,
-    Res =< 360,
-    write('Рейс:'), write(F), nl,
-    write('Город:'), write(City), nl,
-    write('Время:'), write(H1), write(':'), write(M1), nl,
-    write('Дата:'), write(D1), write('-'), write(Mon1), nl,
-    write('Стоимость:'), write(Cost), nl,
-    append(plane(F, City, time(H1, M1), date(D1, Mon1), Cost), L, L).
+    list(time(T1, T2), date(D1, D2), City, Lflight),
+    print_list(Lflight).
 
 
-comp(time(H1, M1), date(D1, M1), time(H2, M2), date(D2, M2), Rest):-
-    compd(date(D1, M1), date(D2, M2), Resd),
+
+
+list(time(H, M), date(D, Mon), City, L2):-
+    findall(plane(X, City, time(Y, Z), date(W, Q), E), plane(X, City, time(Y, Z), date(W, Q), E), L),
+    find_near_flight(time(H, M), date(D, Mon), L, L1),
+    minim(L1, L2).
+
+
+comp(time(H1, M1), date(D1, Mon1), time(H2, M2), date(D2, Mon2), Rest):-
+    compd(date(D1, Mon1), date(D2, Mon2), Resd),
     handler(Resd, time(H1, M1), time(H2, M2), Rest).
 
 handler(0, time(H1, M1), time(H2, M2), Rest):-
-    compt(time(H1, M1), time(H2, M2), Rest).
+    compt(time(H1, M1), time(H2, M2), Rest),!.
 handler(1, time(H1, M1), time(H2, M2), Rest):-
     Hb is H2 + 24,
-    compt(time(H1, M1), time(Hb, M2), Rest).
+    compt(time(H1, M1), time(Hb, M2), Rest),!.
 
 compt(time(H1, M1), time(H2, M2), R):-
-    R is (H2-H1)*60 + (M2-M1).
+    R is (H2-H1)*60 + (M2-M1),!.
 
-compd(date(31, 1), date(1, 2), 1).
-compd(date(28, 2), date(1, 3), 1).
-compd(date(31, 3), date(1, 4), 1).
-compd(date(30, 4), date(1, 5), 1).
-compd(date(31, 5), date(1, 6), 1).
-compd(date(30, 6), date(1, 7), 1).
-compd(date(31, 7), date(1, 8), 1).
-compd(date(31, 8), date(1, 9), 1).
-compd(date(30, 9), date(1, 10), 1).
-compd(date(31, 10), date(1, 11), 1).
-compd(date(30, 11), date(1, 12), 1).
-compd(date(31, 12), date(1, 1), 1).
+compd(date(31, 1), date(1, 2), 1):-!.
+compd(date(28, 2), date(1, 3), 1):-!.
+compd(date(31, 3), date(1, 4), 1):-!.
+compd(date(30, 4), date(1, 5), 1):-!.
+compd(date(31, 5), date(1, 6), 1):-!.
+compd(date(30, 6), date(1, 7), 1):-!.
+compd(date(31, 7), date(1, 8), 1):-!.
+compd(date(31, 8), date(1, 9), 1):-!.
+compd(date(30, 9), date(1, 10), 1):-!.
+compd(date(31, 10), date(1, 11), 1):-!.
+compd(date(30, 11), date(1, 12), 1):-!.
+compd(date(31, 12), date(1, 1), 1):-!.
 compd(date(D1, M1), date(D2, M2), R):-
     (M1 \== M2)->
-    R is -1;
-    R is D2 - D1.
+    R is -1,!;
+    R is D2 - D1,!.
 
+
+find_near_flight(time(_, _), date(_, _), [], []).
+find_near_flight(time(H, M), date(D, Mon), [plane(Flight, City, time(Hp, Mp), date(Dp, Monp), Cost)|T],Lr):-
+     find_near_flight(time(H, M), date(D, Mon), T, Lr1),
+     (comp(time(H, M), date(D, Mon), time(Hp, Mp), date(Dp, Monp), Rest),
+     (Rest >= 0,
+     Rest =< 360)->
+     append([plane(Flight, City, time(Hp, Mp), date(Dp, Monp), Cost)], Lr1, Lr);
+     Lr1 = Lr).
+
+minim([plane(Flight, City, time(H, M), date(D, Mon), Cost)|T], L):-
+    min([plane(Flight, City, time(H, M), date(D, Mon), Cost)|T], Cost1),
+    setof(plane(F, City, time(H1, M1), date(D1, Mon1), Cost1), plane(F, City, time(H1, M1), date(D1, Mon1), Cost1), L).
+
+min([plane(_, _, time(_, _), date(_, _), Cost)|T], Res):-
+    min(T, Res),
+    Res < Cost,!.
+min([plane(_, _, time(_, _), date(_, _), Cost)|_], Cost).
+
+
+
+
+print_list([]):-!.
+print_list([plane(Flight, City, time(Hour, Min), date(Day, Month), Cost)|T]):-
+     write('Рейс: '), write(Flight), nl,
+     write('Город: '), write(City), nl,
+     write('Время: '), write(Hour), write(':'), write(Min), nl,
+     write('День: '), write(Day), write('-'), write(Month), nl,
+     write('Стоимость: '), write(Cost), nl,
+     write('-------------------------'), nl,
+     print_list(T).
